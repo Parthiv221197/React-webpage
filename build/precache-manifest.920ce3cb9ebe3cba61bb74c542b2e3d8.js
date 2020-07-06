eba7a82016b6bf6f5609eb91f0f6598f0ee00014
Parthiv221197 <Parthiv221197@gmail.com>
2020-07-06 15:42:31 -0400

    First version

---
+++ build/precache-manifest.920ce3cb9ebe3cba61bb74c542b2e3d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0880fe3fd9cff4b2df06e7f64a3cac8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d292df68b926bd79ab4d",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bdc29782bec8fbde9789",
+    "url": "/static/css/main.ae9976c5.chunk.css"
+  },
+  {
+    "revision": "d292df68b926bd79ab4d",
+    "url": "/static/js/2.c93e3cc4.chunk.js"
+  },
+  {
+    "revision": "fea4e98622268b2f0cf3f0df55dd1915",
+    "url": "/static/js/2.c93e3cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc29782bec8fbde9789",
+    "url": "/static/js/main.20c60522.chunk.js"
+  },
+  {
+    "revision": "3a2383183f24fb4a8cce",
+    "url": "/static/js/runtime-main.008ef387.js"
+  },
+  {
+    "revision": "599035674a26fbaa1f88e79aafbd924b",
+    "url": "/static/media/woman_teeth_dentist-1200x628-facebook.59903567.jpg"
+  }
+]);
---
+++ src/components/ListAppointments.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { FaTimes } from "react-icons/fa";
+import Moment from 'react-moment';
+
+class ListAppointments extends Component{
+    render(){
+        return(
+
+            <div className="appointment-list item-list mb-3">
+                {this.props.appointments.map( item => (
+                    <div className="patient-item col media py-3" key={item.aptId}>
+                    <div className="mr-3">
+                      <button className="patient-delete btn btn-sm btn-danger" onClick={() => this.props.deleteAppointment(item)}>
+                          <FaTimes />
+                      </button>
+                    </div>
+        
+                    <div className="patient-info media-body" id="hov">
+                      <div className="patient-head d-flex">
+                        <span 
+                            className="patient-name" 
+                            contentEditable 
+                            suppressContentEditableWarning 
+                            onBlur={
+                                e => this.props.updateInfo('patientName', e.target.innerText,item.aptId)
+                            }
+                        >{item.patientName}</span>
+                        <span className="apt-date ml-auto">
+                            <Moment 
+                                date={item.aptDate}
+                                parse="YYYY-MM-dd hh:mm"
+                                format="MMM-DD h:mma" 
+                            />
+                        </span>
+                      </div>
+        
+                      <div className="Address">
+                        <span className="label-item">Address:  </span>
+                        <span 
+                            className="Address"
+                            contentEditable 
+                            suppressContentEditableWarning 
+                            onBlur={
+                                e => this.props.updateInfo('Address', e.target.innerText,item.aptId)
+                            }
+                        >{item.Address}</span>
+                      </div>
+                      <div 
+                        className="apt-notes"
+                        contentEditable 
+                        suppressContentEditableWarning 
+                        onBlur={
+                            e => this.props.updateInfo('aptNotes', e.target.innerText,item.aptId)
+                        }
+                        >{item.aptNotes}</div>
+                    </div>
+                  </div>
+
+                ))}
+        </div>
+        )
+    }    
+}
+
+export default ListAppointments;
